48f3e7a85c9588a13b1b0b42b6f32f934cfcfcdd
Filip Andjelic <filip.andjelic.private@gmail.com>
2021-05-11 16:32:41 +0200

    We have first working version of package!

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
 import * as React from "react";
 import {Loader} from "./components/Loader/Loader";
 
-export type Props = { text: string };
-
-class ExampleComponent extends React.Component<Props> {
-    render() {
-        const { text } = this.props;
-
-        return <div style={{ color: "red" }}>Hello {text}</div>;
-    }
-}
-
 export const Devkit:any = {
-    ExampleComponent: ExampleComponent,
     LoaderComponent: Loader
 };
