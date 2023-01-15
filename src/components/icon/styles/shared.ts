4033d8944a85115a18f348b4e2d3254e19d582b5
Sinisa B <sinisa.becic@outlook.com>
2023-01-15 23:57:23 +0100

    feat: additional customizing of icon component

--- src/components/icon/styles/shared.ts
+++ src/components/icon/styles/shared.ts
@@ -2,12 +2,17 @@ import { css } from "styled-components";
 import { IconProps } from "../types";
 
 export const sharedPath = css<IconProps>`
-  "color": ${(props: IconProps) => props.color || props.theme?.palette?.main.one || "#7E8A96"};
-  "fill": ${(props: IconProps) => props.fill || props.theme?.palette?.main.one || "#7E8A96"};
+  color: ${(props) => props.color || props.theme?.palette?.main.one || "#fff"};
+  fill: ${(props) => props.color || props.theme?.palette?.main.one || "#fff"};
 `;
 
 export const sharedSvg = css<IconProps>`
-"width": ${(props: IconProps) => props.width || 24};
-"height": ${(props: IconProps) => props.height || props.width || 24};
+  position: ${(props) => props.position || "absolute"};
+  width: ${(props) => props.width || 24};
+  height: ${(props) => props.height || props.width || 24};
+  top: ${(props) => props.top || "0"};
+  bottom: ${(props) => props.bottom || "0"};
+  right: ${(props) => props.right || "0"};
+  left: ${(props) => props.left || "0"};
   ${sharedPath}
 `;
