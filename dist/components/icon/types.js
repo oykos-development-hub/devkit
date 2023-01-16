163a9a0ff5486dfdcf269b05840b0c0f395c1655
Sinisa B <sinisa.becic@outlook.com>
2023-01-16 10:45:24 +0100

    refactor: additional customization of icon component

--- dist/components/icon/types.js
+++ dist/components/icon/types.js
@@ -5,4 +5,9 @@ export var Directions;
     Directions["right"] = "right";
     Directions["top"] = "top";
 })(Directions || (Directions = {}));
+export var Positions;
+(function (Positions) {
+    Positions["absolute"] = "absolute";
+    Positions["relative"] = "relative";
+})(Positions || (Positions = {}));
 //# sourceMappingURL=types.js.map
--- src/components/icon/types.ts
+++ src/components/icon/types.ts
@@ -7,13 +7,22 @@ export enum Directions {
   top = "top",
 }
 
+export enum Positions {
+  absolute = "absolute",
+  relative = "relative",
+}
+
 export interface IconProps {
-  fill?: string;
   color?: string;
   width?: string | number;
   height?: string | number;
   onClick?: () => void;
   className?: string;
   direction?: Directions | keyof typeof Directions;
+  top?: string;
+  bottom?: string;
+  right?: string;
+  left?: string;
+  position?: Positions | keyof typeof Positions;
   theme?: DefaultTheme;
 }
