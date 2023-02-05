67b50ae979bfb75039209399a160ebf411983a58
Sinisa B <sinisa.becic@outlook.com>
2023-02-05 23:00:43 +0100

    fix: dist & storybook docs fix

--- dist/types.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./index";
-export * from "./components/loader/types";
-export * from "./components/typography/types";
-export * from "./shared/theme/types";
-export * from "./components/button/types";
