f09f70f0721042435f152cf0af69dcdc1a97a621
Sinisa B <sinisa.becic@outlook.com>
2023-02-07 10:10:10 +0100

    run pull request

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+export declare const Button: ({ onClick, content, disabled, variant, size, style, theme, }: import("./types").ButtonProps) => JSX.Element;
+export declare const Loader: (props: import("./types").LoaderProps) => import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>>;
+export declare const Typography: (props: import("./types").TypographyProps) => import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>>;
+export * from "./components/icon";
