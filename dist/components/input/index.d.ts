ec47bc15c2cd76eeef60229c0d66d514df9078ab
Sinisa B <sinisa.becic@outlook.com>
2023-02-14 02:37:24 +0100

    clean: old build delete

--- dist/components/input/index.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { InputProps } from "./types";
-export declare const Input: ({ name, value, theme, style, disabled, label, textarea, leftContent, rightContent, error, hint, placeholder, onChange, onBlur, onFocus, id, inputRef, ...props }: InputProps) => React.ReactElement;
