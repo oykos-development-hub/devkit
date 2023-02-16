a5595b6a5c3ff9ed8f4b3c966845d0e6edd2af4a
Sinisa B <sinisa.becic@outlook.com>
2023-02-16 10:11:46 +0100

    update typescript types

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Typography as TypographyComponent } from "./components/typography";
 import { Input as InputComponent } from "./components/input";
 import { Pin as PinComponent } from "./components/pin";
 import { Checkbox as CheckboxComponent } from "./components/checkbox";
+import { Radio as RadioComponent } from "./components/radio";
 
 export const Button = ButtonComponent;
 export const Loader = LoaderComponent;
@@ -11,6 +12,7 @@ export const Typography = TypographyComponent;
 export const Input = InputComponent;
 export const Pin = PinComponent;
 export const Checkbox = CheckboxComponent;
+export const Radio = RadioComponent;
 
 export * from "./components/icon";
 export * from "./types";
