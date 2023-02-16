4b93a896c030ff9a7df37ed990e575f24d6d97cd
Sinisa B <sinisa.becic@outlook.com>
2023-02-16 09:43:56 +0100

    clean: old dist

--- dist/components/icon/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as AssignIcon } from "./variations/AssignIcon";
-export { default as XIcon } from "./variations/XIcon";
-export { default as DangerIcon } from "./variations/DangerIcon";
-export { default as CircleCheckIcon } from "./variations/CircleCheckIcon";
