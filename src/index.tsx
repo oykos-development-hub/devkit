c1ee91d4064783e5559fdd57f2438a94fc6472d2
Sinisa B <sinisa.becic@outlook.com>
2023-02-20 17:18:58 +0100

    add alert to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import { Loader as LoaderComponent } from "./components/loader";
 import { Typography as TypographyComponent } from "./components/typography";
 import { Input as InputComponent } from "./components/input";
 import { Pin as PinComponent } from "./components/pin";
+import { Alert as AlertComponent } from "./components/alert";
 
 export const Button = ButtonComponent;
 export const Loader = LoaderComponent;
 export const Typography = TypographyComponent;
 export const Input = InputComponent;
 export const Pin = PinComponent;
+export const Alert = AlertComponent;
 
 export * from "./components/icon";
 export * from "./types";
