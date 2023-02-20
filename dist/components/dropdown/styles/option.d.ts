4d39548e89b6b9bc15f41d5951cb8a2d4e0ac5cd
Sinisa B <sinisa.becic@outlook.com>
2023-02-20 16:40:43 +0100

    clean old build

--- dist/components/dropdown/styles/option.d.ts
+++
@@ -1,5 +0,0 @@
-import { DefaultTheme } from "styled-components";
-export declare const Option: import("styled-components").StyledComponent<"div", DefaultTheme, {
-    theme: DefaultTheme;
-    isDisabled: boolean;
-}, never>;
