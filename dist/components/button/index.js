76fcb482d2e8aafdc61df72b777fa7b7a8b22b31
Sinisa B <sinisa.becic@outlook.com>
2023-02-20 17:21:12 +0100

    run git commit

---
+++ dist/components/button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ButtonSizes, ButtonVariants } from "./types";
+import { Container } from "./styles/container";
+import { Content } from "./styles/content";
+import { Theme } from "../../shared/theme";
+export var Button = function (_a) {
+    var onClick = _a.onClick, content = _a.content, _b = _a.disabled, disabled = _b === void 0 ? false : _b, _c = _a.variant, variant = _c === void 0 ? ButtonVariants.primary : _c, _d = _a.size, size = _d === void 0 ? ButtonSizes.lg : _d, style = _a.style, theme = _a.theme;
+    return (React.createElement(Container, { style: style, disabled: disabled, variant: variant, size: size, onClick: onClick, theme: theme || Theme },
+        React.createElement(Content, { size: size, disabled: disabled, variant: variant, theme: theme || Theme, customStyle: style }, content)));
+};
+//# sourceMappingURL=index.js.map
