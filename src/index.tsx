74d62180d383569c9b5a9957b73bd17031e24d34
masalakovic <94193799+masalakovic@users.noreply.github.com>
2023-02-23 15:50:17 +0100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import { Loader as LoaderComponent } from "./components/loader";
 import { Typography as TypographyComponent } from "./components/typography";
 import { Input as InputComponent } from "./components/input";
 import { Pin as PinComponent } from "./components/pin";
+import { Pagination as PaginationComponent } from "./components/paginate";
 
 export const Button = ButtonComponent;
 export const Loader = LoaderComponent;
 export const Typography = TypographyComponent;
 export const Input = InputComponent;
 export const Pin = PinComponent;
+export const Pagination = PaginationComponent;
 
 export * from "./components/icon";
 export * from "./types";
