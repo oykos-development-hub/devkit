d42c301e3af9d45c27fdc01955141805f7c2ee46
Sinisa B <sinisa.becic@outlook.com>
2023-02-27 15:40:07 +0100

    fix: removed unused props

--- src/components/datepicker/types.ts
+++ src/components/datepicker/types.ts
@@ -1,12 +1,7 @@
-import { FormatFunction } from "@datepicker-react/styled";
 import React from "react";
 import { DefaultTheme } from "styled-components";
 
 export interface DatepickerTypes {
-  focusedInput: string;
-  displayFormat?: string | FormatFunction;
-  showCalendarIcon?: boolean;
-  onClose?(): void;
   style?: React.CSSProperties;
   theme?: DefaultTheme;
 }
