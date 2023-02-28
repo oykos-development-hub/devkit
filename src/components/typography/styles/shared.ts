0f51230b6bf0c0901e3c19b14b7d571201a248f5
Sinisa B <sinisa.becic@outlook.com>
2023-02-28 10:45:29 +0100

    refactor code, fix font size and font weight in props

--- src/components/typography/styles/shared.ts
+++ src/components/typography/styles/shared.ts
@@ -2,11 +2,12 @@ import { css } from "styled-components";
 import { TypographyProps } from "../types";
 import { Theme } from "../../../shared/theme";
 
-export const shared = css<TypographyProps>`
-  color: ${(props: TypographyProps) => props.style?.color || props?.theme?.palette?.gray900 || Theme.palette?.gray900};
-  display: block;
-  font-family: ${(props: TypographyProps) =>
-    props.style?.fontFamily || props.theme?.fontFamily?.one || Theme.fontFamily?.one};
-`;
+export const themeToUse = (props: TypographyProps) => props.theme || Theme;
+export const fontSize = (props: TypographyProps) => props.style?.fontSize || props.fontSize;
+export const fontWeight = (props: TypographyProps) => props.style?.fontWeight || props.fontWeight;
 
-export const fontWeight = (props: TypographyProps) => props.fontWeight || Theme.fontWeight.regular;
+export const shared = css<TypographyProps>`
+  display: block;
+  color: ${(props: TypographyProps) => props.style?.color || themeToUse(props).palette?.gray900};
+  font-family: ${(props: TypographyProps) => props.style?.fontFamily || themeToUse(props).fontFamily?.one};
+`;
