3615462185b3a743332b6f4d37b85630a85a79df
Sinisa B <sinisa.becic@outlook.com>
2023-02-28 15:20:28 +0100

    remove unnecessary props

--- src/components/typography/styles/shared.ts
+++ src/components/typography/styles/shared.ts
@@ -3,12 +3,11 @@ import { TypographyProps } from "../types";
 import { Theme } from "../../../shared/theme";
 
 export const themeToUse = (props: TypographyProps) => props.theme || Theme;
-export const fontSize = (props: TypographyProps) => props.style?.fontSize || props.fontSize;
-export const fontWeight = (props: TypographyProps) => props.style?.fontWeight || props.fontWeight;
+export const fontSize = (props: TypographyProps) => props.style?.fontSize;
+export const fontWeight = (props: TypographyProps) => props.style?.fontWeight;
 
 export const shared = css<TypographyProps>`
   display: block;
   color: ${(props: TypographyProps) => props.style?.color || themeToUse(props).palette?.gray900};
-  font-family: ${(props: TypographyProps) =>
-    props.style?.fontFamily || props.fontFamily || themeToUse(props).fontFamily?.one};
+  font-family: ${(props: TypographyProps) => props.style?.fontFamily || themeToUse(props).fontFamily?.one};
 `;
--- src/components/typography/types.ts
+++ src/components/typography/types.ts
@@ -1,13 +1,10 @@
 import React from "react";
-import { TypographyVariants as Variants, WeightVariants } from "./variants";
+import { TypographyVariants as Variants } from "./variants";
 import { DefaultTheme } from "styled-components";
 
 export interface TypographyProps {
   content: React.ReactNode | string;
   variant?: Variants | string;
-  fontFamily?: string;
-  fontWeight?: WeightVariants | string;
-  fontSize?: string;
   onClick?: (e?: React.MouseEvent<any>) => any;
   theme?: DefaultTheme;
   style?: React.CSSProperties;
