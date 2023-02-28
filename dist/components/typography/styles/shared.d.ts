11d0f138b31db46e1a5ff3e6159518199247bd2f
Sinisa B <sinisa.becic@outlook.com>
2023-02-28 10:47:35 +0100

    build: run git commit

--- dist/components/typography/styles/shared.d.ts
+++ dist/components/typography/styles/shared.d.ts
@@ -1,3 +1,5 @@
 import { TypographyProps } from "../types";
+export declare const themeToUse: (props: TypographyProps) => import("styled-components").DefaultTheme;
+export declare const fontSize: (props: TypographyProps) => string | number | undefined;
+export declare const fontWeight: (props: TypographyProps) => string | (number & {}) | undefined;
 export declare const shared: import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<TypographyProps, import("styled-components").DefaultTheme>>;
-export declare const fontWeight: (props: TypographyProps) => string;
--- dist/components/typography/styles/shared.js
+++ dist/components/typography/styles/shared.js
@@ -1,9 +1,11 @@
 import { css } from "styled-components";
 import { Theme } from "../../../shared/theme";
+export const themeToUse = (props) => props.theme || Theme;
+export const fontSize = (props) => { var _a; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.fontSize) || props.fontSize; };
+export const fontWeight = (props) => { var _a; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.fontWeight) || props.fontWeight; };
 export const shared = css `
-  color: ${(props) => { var _a, _b, _c, _d; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.color) || ((_c = (_b = props === null || props === void 0 ? void 0 : props.theme) === null || _b === void 0 ? void 0 : _b.palette) === null || _c === void 0 ? void 0 : _c.gray900) || ((_d = Theme.palette) === null || _d === void 0 ? void 0 : _d.gray900); }};
   display: block;
-  font-family: ${(props) => { var _a, _b, _c, _d; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.fontFamily) || ((_c = (_b = props.theme) === null || _b === void 0 ? void 0 : _b.fontFamily) === null || _c === void 0 ? void 0 : _c.one) || ((_d = Theme.fontFamily) === null || _d === void 0 ? void 0 : _d.one); }};
+  color: ${(props) => { var _a, _b; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.color) || ((_b = themeToUse(props).palette) === null || _b === void 0 ? void 0 : _b.gray900); }};
+  font-family: ${(props) => { var _a, _b; return ((_a = props.style) === null || _a === void 0 ? void 0 : _a.fontFamily) || ((_b = themeToUse(props).fontFamily) === null || _b === void 0 ? void 0 : _b.one); }};
 `;
-export const fontWeight = (props) => props.fontWeight || Theme.fontWeight.regular;
 //# sourceMappingURL=shared.js.map
