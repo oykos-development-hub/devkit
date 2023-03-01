2274e03429736182c10420904b58d0838ca14347
masalakovic <94193799+masalakovic@users.noreply.github.com>
2023-03-01 09:32:06 +0100

    dist upload

--- dist/components/dropdown/styles/option.js
+++ dist/components/dropdown/styles/option.js
@@ -1,23 +1,23 @@
 import styled, { css } from "styled-components";
-export const Option = styled.div(({ theme, isDisabled }) => {
+export const Option = styled.div(({ theme, isDisabled, style }) => {
     const { gray900, gray600 } = theme.palette;
     return css `
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    & * {
-      color: ${isDisabled ? gray600 : gray900};
-    }
-    & > div:first-child {
       display: flex;
       align-items: center;
-      gap: 1em;
-    }
+      justify-content: space-between;
 
-    & .option-icon {
-      display: flex;
-    }
-  `;
+      & * {
+        color: ${isDisabled ? gray600 : (style === null || style === void 0 ? void 0 : style.color) || gray900};
+      }
+      & > div:first-child {
+        display: flex;
+        align-items: center;
+        gap: 1em;
+      }
+
+      & .option-icon {
+        display: flex;
+      }
+    `;
 });
 //# sourceMappingURL=option.js.map
