89b68d6ecb457fdc637060bf26f83bcdfe931d60
Sinisa B <sinisa.becic@outlook.com>
2023-03-01 14:38:21 +0100

    build: run git commit

--- dist/components/checkbox/index.js
+++ dist/components/checkbox/index.js
@@ -3,7 +3,7 @@ import { CheckboxSize } from "./types";
 import { Container } from "./styles/container";
 import { Content, HiddenCheckbox, Icon } from "./styles/content";
 import { Theme } from "../../shared/theme";
-import { CheckIcon } from "./checkIcon";
+import { CheckIcon } from "../icon";
 export const Checkbox = ({ checked = false, size = CheckboxSize.sm, onClick, style, theme = Theme, disabled = false, }) => {
     return (React.createElement(Container, null,
         React.createElement(HiddenCheckbox, { checked: checked }),
