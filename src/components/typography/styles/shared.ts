b0d61af92cbdfbb9f42f8b4bc29e8c810ad9f821
Sinisa B <sinisa.becic@outlook.com>
2023-03-01 10:42:52 +0100

    refactor: typography text formatting

--- src/components/typography/styles/shared.ts
+++ src/components/typography/styles/shared.ts
@@ -2,12 +2,9 @@ import { css } from "styled-components";
 import { TypographyProps } from "../types";
 import { Theme } from "../../../shared/theme";
 
-export const themeToUse = (props: TypographyProps) => props.theme || Theme;
-export const fontSize = (props: TypographyProps) => props.style?.fontSize;
-export const fontWeight = (props: TypographyProps) => props.style?.fontWeight;
-
 export const shared = css<TypographyProps>`
   display: block;
-  color: ${(props: TypographyProps) => props.style?.color || themeToUse(props).palette?.gray900};
-  font-family: ${(props: TypographyProps) => props.style?.fontFamily || themeToUse(props).fontFamily?.one};
+  color: ${(props: TypographyProps) => props.style?.color || props.theme?.palette?.gray900 || Theme.palette?.gray900};
+  font-family: ${(props: TypographyProps) =>
+    props.style?.fontFamily || props.theme?.fontFamily?.one || Theme.fontFamily.one};
 `;
