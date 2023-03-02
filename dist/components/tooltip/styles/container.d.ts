960c8d5c5daf187f8d2255b5c13bbae84cb52ad9
Sinisa B <sinisa.becic@outlook.com>
2023-03-02 16:44:40 +0100

    delete old dist

--- dist/components/tooltip/styles/container.d.ts
+++
@@ -1,4 +0,0 @@
-import { Positions } from "../types";
-export declare const Container: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
-    position: keyof typeof Positions;
-}, never>;
