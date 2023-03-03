2fb3e3d08f9bc1cadbc15b1eb62e1fa5e0cb0bc8
Sinisa B <sinisa.becic@outlook.com>
2023-03-03 15:56:17 +0100

    delete

--- dist/components/checkbox/checkIcon.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-export const CheckIcon = () => React.createElement("path", { d: "M10 3L4.5 8.5L2 6" });
-//# sourceMappingURL=checkIcon.js.map
--- dist/components/checkbox/index.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-declare const _default: ComponentMeta<React.FC<import("./types").CheckboxProps>>;
-export default _default;
-export declare const Default: ComponentStory<React.FC<import("./types").CheckboxProps>>;
