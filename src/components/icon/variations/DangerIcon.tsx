ef797f2bfdc828211138de736d7128befcc05c2f
Sinisa B <sinisa.becic@outlook.com>
2023-03-16 12:17:32 +0100

    optimize danger icon for input component

--- src/components/icon/variations/DangerIcon.tsx
+++ src/components/icon/variations/DangerIcon.tsx
@@ -3,14 +3,8 @@ import { IconProps } from "../types";
 import { Path, Svg } from "../styles/shared";
 
 const DangerIcon = (props: IconProps): React.ReactElement => (
-  <Svg viewBox="0 0 20 20" onClick={props.onClick} {...props}>
-    <Path d="M10 4a1 1 0 011 1v6a1 1 0 11-2 0V5a1 1 0 011-1zM10 14a1 1 0 100 2 1 1 0 000-2z" {...props} />
-    <Path
-      fillRule="evenodd"
-      d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zM2 10a8 8 0 1016 0 8 8 0 00-16 0z"
-      clipRule="evenodd"
-      {...props}
-    />
+  <Svg viewBox="0 0 21 20" onClick={props.onClick} {...props}>
+    <Path d="M10.5 6.5V10m0 3.5h.009M19.25 10a8.75 8.75 0 11-17.5 0 8.75 8.75 0 0117.5 0z" {...props} />
   </Svg>
 );
 
