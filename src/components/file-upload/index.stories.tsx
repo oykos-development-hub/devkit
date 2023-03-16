0f8eea6c100131e5f7da3811395b4213e697f38a
Sinisa B <sinisa.becic@outlook.com>
2023-03-16 02:20:41 +0100

    small fix

--- src/components/file-upload/index.stories.tsx
+++ src/components/file-upload/index.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { FileUpload } from "./index";
 import { StoryWrapper } from "../../shared/components/story-wrapper";
