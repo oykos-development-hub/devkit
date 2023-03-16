bbafe4105fbfab8726ab3efe887022e1ba2be82e
Sinisa B <sinisa.becic@outlook.com>
2023-03-16 02:20:17 +0100

    small fix

--- src/components/file-upload/types.ts
+++ src/components/file-upload/types.ts
@@ -1,6 +1,5 @@
 import React, { ReactElement, ReactNode } from "react";
 import { DefaultTheme } from "styled-components";
-import { FileWithPath } from "react-dropzone";
 
 export interface FileUploadProps {
   onFileUpload: (files: File[]) => void;
