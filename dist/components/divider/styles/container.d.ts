7a95cb12621cdf25f002fc86f0a9ce02456b675e
Sinisa B <sinisa.becic@outlook.com>
2023-04-06 03:59:34 +0200

    build: run git commit

---
+++ dist/components/divider/styles/container.d.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+import React from "react";
+export declare const Container: import("styled-components").StyledComponent<"hr", DefaultTheme, {
+    color?: string | undefined;
+    width?: string | undefined;
+    height?: string | undefined;
+    style?: React.CSSProperties | undefined;
+    theme?: DefaultTheme | undefined;
+}, never>;
---
+++ dist/components/icon/variations/UserIcon.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Path, Svg } from "../styles/shared";
+const UserIcon = (props) => (React.createElement(Svg, Object.assign({ viewBox: "0 0 24 24", onClick: props.onClick, fill: props.fill }, props),
+    React.createElement(Path, Object.assign({ d: "M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2M16 7a4 4 0 11-8 0 4 4 0 018 0z", stroke: props.stroke }, props))));
+export default UserIcon;
+//# sourceMappingURL=UserIcon.js.map
