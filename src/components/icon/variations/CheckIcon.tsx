6e7b570a3f6d95164e210801147472c3a02ea2ed
Sinisa B <sinisa.becic@outlook.com>
2023-04-10 00:40:58 +0200

    fix

--- src/components/icon/variations/CheckIcon.tsx
+++ src/components/icon/variations/CheckIcon.tsx
@@ -3,8 +3,15 @@ import { IconProps } from "../types";
 import { Path, Svg } from "../styles/shared";
 
 const CheckIcon = (props: IconProps): React.ReactElement => (
-  <Svg viewBox="0 0 12 12" onClick={props.onClick} {...props}>
-    <Path d="M10 3L4.5 8.5 2 6" {...props} />
+  <Svg viewBox="0 0 18 13" onClick={props.onClick} fill={props.fill || "none"} {...props}>
+    <Path
+      d="M17 1L6 12 1 7"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      stroke={props.stroke || "#000"}
+      {...props}
+    />
   </Svg>
 );
 
