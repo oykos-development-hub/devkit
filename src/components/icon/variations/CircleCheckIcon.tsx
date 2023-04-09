a7e025fc4cd3ba41731fe4498ee5e8550d36a86c
Sinisa B <sinisa.becic@outlook.com>
2023-04-10 01:03:22 +0200

    fix

--- src/components/icon/variations/CircleCheckIcon.tsx
+++ src/components/icon/variations/CircleCheckIcon.tsx
@@ -3,13 +3,13 @@ import { IconProps } from "../types";
 import { Path, Svg } from "../styles/shared";
 
 export const CircleCheckIcon = (props: IconProps): React.ReactElement => (
-  <Svg viewBox="0 0 23 22" onClick={props.onClick} fill={props.fill} {...props}>
-    <Path d="M9.743 15.314L5.5 11.07l1.414-1.414 2.829 2.828 5.656-5.657 1.415 1.415-7.071 7.07z" {...props} />
+  <Svg viewBox="0 0 22 23" onClick={props.onClick} fill={props.fill || "none"} {...props}>
     <Path
-      fillRule="evenodd"
-      d="M.5 11c0-6.075 4.925-11 11-11s11 4.925 11 11-4.925 11-11 11-11-4.925-11-11zm11 9a9 9 0 110-18 9 9 0 010 18z"
-      clipRule="evenodd"
-      stroke={props.stroke}
+      d="M21 11.08V12a10 10 0 11-5.93-9.14M21 4L11 14.01l-3-3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      stroke={props.stroke || "#000"}
       {...props}
     />
   </Svg>
