5257313f15b1ef5a45e1bd8819e75b7f068c5421
Siniša B <sinisa.becic@outlook.com>
2023-04-13 11:26:15 +0200

    added checkbox function to handle 'checked' state

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CheckboxProps, CheckboxSize } from "./types";
 import { Container } from "./styles/container";
 import { Content, HiddenCheckbox } from "./styles/content";
@@ -6,18 +6,26 @@ import { Theme } from "../../shared/theme";
 import { CheckIcon } from "../icon";
 
 export const Checkbox: React.FC<CheckboxProps> = ({
-  checked = false,
   size = CheckboxSize.sm,
   onClick,
   style,
   theme = Theme,
   disabled = false,
 }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleClick = (e: any) => {
+    if (disabled) return;
+
+    setIsChecked(!isChecked);
+    onClick && onClick(e);
+  };
+
   return (
     <Container>
-      <HiddenCheckbox checked={checked} />
-      <Content checked={checked} onClick={onClick} size={size} style={style} theme={theme} disabled={disabled}>
-        {checked && <CheckIcon stroke={disabled ? theme.palette.gray300 : theme.palette.white} />}
+      <HiddenCheckbox checked={isChecked} />
+      <Content checked={isChecked} onClick={handleClick} size={size} style={style} theme={theme} disabled={disabled}>
+        {isChecked && <CheckIcon stroke={disabled ? theme.palette.gray300 : theme.palette.white} />}
       </Content>
     </Container>
   );
