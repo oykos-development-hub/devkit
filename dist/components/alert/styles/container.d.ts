0b2d4e4f106f9f5ff3338683dfd7c3afacde34b6
Siniša B <sinisa.becic@outlook.com>
2023-04-19 00:35:15 +0200

    build: run git commit

--- dist/components/alert/styles/container.d.ts
+++ dist/components/alert/styles/container.d.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { DefaultTheme } from "styled-components";
 import { AlertVariants } from "../types";
 export declare const Container: import("styled-components").StyledComponent<"div", DefaultTheme, {
-    variant: AlertVariants | string;
-    theme: DefaultTheme;
-    style: React.CSSProperties | undefined;
+    variant: AlertVariants | `${AlertVariants}`;
+    theme?: DefaultTheme | undefined;
+    style?: React.CSSProperties | undefined;
 }, never>;
--- dist/components/alert/styles/wrapper.js
+++ dist/components/alert/styles/wrapper.js
@@ -1,10 +1,19 @@
 import styled, { css } from "styled-components";
 import { rem } from "polished";
-export const Wrapper = styled.div(() => ({ size }) => {
+import { Content } from "./content";
+export const Wrapper = styled.div(() => ({ variant = "success", size, theme }) => {
     const padding = {
         sm: rem("8px"),
         md: rem("12px"),
         lg: rem("16px"),
+    };
+    const { primary600, success600, secondary600, warning600, error600 } = theme.palette;
+    const bgColor = {
+        primary: primary600,
+        info: secondary600,
+        success: success600,
+        warning: warning600,
+        error: error600,
     };
     return css `
     display: flex;
@@ -15,6 +24,15 @@ export const Wrapper = styled.div(() => ({ size }) => {
     word-wrap: break-word;
     padding: ${padding[size]};
     width: 100%;
+
+    & ${Content}:nth-child(2) {
+      padding: ${rem("6px")};
+      border-radius: 0.125rem;
+
+      &:hover {
+        background-color: ${bgColor[variant]};
+      }
+    }
   `;
 });
 //# sourceMappingURL=wrapper.js.map
