0796ebf3bac0210c368e00bd52becd1399d8afd1
Tijana24 <t.devedzic@gmail.com>
2023-04-19 11:14:15 +0200

    fix for tab hover state

--- dist/components/tabs/style/tab.js
+++ dist/components/tabs/style/tab.js
@@ -1,13 +1,19 @@
 import styled, { css } from "styled-components";
 export const StyledTab = styled.button(({ disabled, theme, style, active, }) => {
-    const { white, gray900 } = theme.palette;
+    const { white, gray400, gray900, primary500 } = theme.palette;
     return css `
       padding: 1em 1.625em;
-      border: 0.25em solid ${active && !disabled ? `#B27D1E` : `dark`};
+      border: 0.25em solid ${active && !disabled ? primary500 : "dark"};
       color: ${active && !disabled ? white : gray900};
-      background-color: ${active && !disabled ? `#B27D1E` : `transparent`};
+      background-color: ${active && !disabled ? primary500 : "transparent"};
       transition: all 0.1s ease-in-out;
       cursor: ${disabled ? "auto" : "pointer"};
+
+      &:hover {
+        color: ${white};
+        border: 0.25em solid ${primary500};
+        background-color: ${disabled ? gray400 : primary500};
+      }
 
       ${Object.assign({}, style)}
     `;
