7b9c0e728294ee93954a360ba98cee76033180ca
petar-dobre <petar.dobre@yahoo.com>
2023-04-20 15:39:09 +0200

    fix: place the StyledTooltip component outside of the Tooltip Container in order to be able to have width bigger than the children provided to the Container element

--- src/components/tooltip/styles/container.ts
+++ src/components/tooltip/styles/container.ts
@@ -1,12 +1,13 @@
 import styled, { css } from "styled-components";
 import { TooltipPositions } from "../types";
 import { tooltipPosition } from "./positions/tooltipPositions";
+import { StyledTooltip } from "./tooltip";
 
 export const Container = styled.div<{ position: TooltipPositions | `${TooltipPositions}` }>(({ position }) => {
   return css`
     position: relative;
 
-    &:hover > div {
+    &:hover + ${StyledTooltip} {
       visibility: visible;
     }
 
