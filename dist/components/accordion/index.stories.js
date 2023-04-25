7cfa177a0ac78e2048ac9780425d891995114783
Tijana24 <t.devedzic@gmail.com>
2023-04-25 13:38:49 +0200

    fix: added custom header and custom content

--- dist/components/accordion/index.stories.js
+++ dist/components/accordion/index.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Accordion } from "./index";
 import { Typography } from "../../index";
 import { Theme } from "../../shared/theme";
-import { MenuIcon, MoreVerticalIcon } from "../icon";
+import { ChevronDownIcon } from "../icon";
 import { StoryWrapper } from "../../shared/components/story-wrapper";
 export default {
     component: Accordion,
@@ -21,26 +21,32 @@ export default {
         },
     },
 };
-const accordionData = [
-    {
-        title: React.createElement(Typography, { variant: "bodySmall", content: "Accordion Title" }),
-        content: (React.createElement(Typography, { variant: "bodySmall", content: "Lorem ipsum dolor sit amet. Aut internos autem et dolorem consequuntur a culpa itaque! Lorem ipsum dolor sit amet. Aut internos autem et dolorem consequuntur a culpa itaque!", style: { color: "#616161" } })),
-    },
-    {
-        title: React.createElement(Typography, { variant: "bodySmall", content: "Accordion 1" }),
-        content: React.createElement(Typography, { variant: "bodySmall", content: "Accordion 1 content.", style: { color: "#616161" } }),
-    },
-];
 const Template = (args) => (React.createElement(StoryWrapper, { style: { display: "grid", alignContent: "center" } },
     React.createElement("div", { style: { width: "400px" } },
-        React.createElement(Accordion, Object.assign({}, args, { data: accordionData })))));
+        React.createElement(Accordion, Object.assign({}, args)))));
 export const Default = Template.bind({});
-export const WithCustomIcon = Template.bind({});
-WithCustomIcon.args = {
-    customIcon: React.createElement(MenuIcon, null),
+Default.args = {
+    data: [
+        {
+            title: React.createElement(Typography, { variant: "bodySmall", content: "Accordion Title" }),
+            content: (React.createElement(Typography, { variant: "bodySmall", content: "Lorem ipsum dolor sit amet. Aut internos autem et dolorem consequuntur a culpa itaque! Lorem ipsum dolor sit amet. Aut internos autem et dolorem consequuntur a culpa itaque!", style: { color: "#616161" } })),
+        },
+        {
+            title: React.createElement(Typography, { variant: "bodySmall", content: "Accordion 1" }),
+            content: React.createElement(Typography, { variant: "bodySmall", content: "Accordion 1 content.", style: { color: "#616161" } }),
+        },
+    ]
 };
-export const WithContextIcon = Template.bind({});
-WithContextIcon.args = {
-    rightCustomIcon: React.createElement(MoreVerticalIcon, null),
+export const CustomHeaderAndContentAccordion = Template.bind({});
+CustomHeaderAndContentAccordion.args = {
+    data: [
+        {
+            customHeader: React.createElement("div", { style: { width: "100%" } },
+                React.createElement("div", { style: { display: "flex", justifyContent: "space-between" } },
+                    "Custom Accordion 1 ",
+                    React.createElement(ChevronDownIcon, null))),
+            customContent: React.createElement("div", { style: { padding: "4px 16px 16px" } }, "test custom content")
+        }
+    ]
 };
 //# sourceMappingURL=index.stories.js.map
