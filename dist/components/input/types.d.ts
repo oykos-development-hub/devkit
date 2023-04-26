2a4b7549ae1c3ab665b17b7282b46864235be947
Siniša B <sinisa.becic@outlook.com>
2023-04-26 16:08:18 +0200

    build: pull-request

--- dist/components/input/types.d.ts
+++ dist/components/input/types.d.ts
@@ -10,9 +10,9 @@ export interface InputProps {
     textarea?: boolean;
     leftContent?: ReactElement;
     rightContent?: ReactElement;
-    error?: ReactNode | string;
-    hint?: ReactNode | string;
-    placeholder?: ReactNode | string;
+    error?: string;
+    hint?: string;
+    placeholder?: string;
     onChange?: ChangeEventHandler<any>;
     onFocus?: FocusEventHandler;
     onBlur?: FocusEventHandler;
