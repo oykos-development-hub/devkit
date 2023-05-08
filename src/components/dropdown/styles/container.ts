3fd1e48a2b2716ff386a8170401c9b8255e45157
Siniša B <sinisa.becic@outlook.com>
2023-05-08 11:33:08 +0200

    dropdown width set to 100%

--- src/components/dropdown/styles/container.ts
+++ src/components/dropdown/styles/container.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { rem } from "polished";
 
 export const Container = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: ${rem("6px")};
