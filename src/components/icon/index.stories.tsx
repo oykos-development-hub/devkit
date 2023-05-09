e04f7a7aa5f459ef8b244916465c55afdba8f044
Siniša B <sinisa.becic@outlook.com>
2023-05-09 14:22:43 +0200

    fix

--- src/components/icon/index.stories.tsx
+++ src/components/icon/index.stories.tsx
@@ -147,146 +147,145 @@ export default {
   },
 } as ComponentMeta<typeof XIcon>;
 
-const First: ComponentStory<typeof XIcon> = (args: IconProps) => (
+const Template: ComponentStory<typeof XIcon> = (args: IconProps) => (
   <StoryWrapper>
-    <LinkIcon {...args} />
-    <FolderIcon {...args} />
-    <LockIcon {...args} />
-    <MenuIconTwo {...args} />
-    <InboxIcon {...args} />
-    <HelpCircleIcon {...args} />
-    <InfoIcon {...args} />
-    <GlobeIcon {...args} />
-    <FolderPlusIcon {...args} />
-    <FolderMinusIcon {...args} />
-    <FileTextIcon {...args} />
-    <FilterIcon {...args} />
-    <CornerUpIcon {...args} />
-    <ExternalIcon {...args} />
-    <EditIconThree {...args} />
-    <EditIconTwo {...args} />
-    <ClipboardIcon {...args} />
-    <DatabaseIcon {...args} />
-    <CopyIcon {...args} />
-    <CloudOffIcon {...args} />
-    <CheckSquareIcon {...args} />
-    <BarChart {...args} />
-    <BarChartTwo {...args} />
-    <BellIcon {...args} />
-    <BellOffIcon {...args} />
-    <ImageIcon {...args} />
-    <ArrowLeftCircleIcon {...args} />
-    <ArrowDownCircleIcon {...args} />
-    <ArrowRightCircleIcon {...args} />
-    <ArchiveIcon {...args} />
-    <AlignRightIcon {...args} />
-    <AlignLeftIcon {...args} />
-    <AlignJustifyIcon {...args} />
-    <AlignCenterIcon {...args} />
-    <AlertTriangleIcon {...args} />
-    <ArrowUpIcon {...args} />
-    <ArrowDownIcon {...args} />
-    <ArrowLeftIcon {...args} />
-    <ArrowRightIcon {...args} />
-    <MailIcon {...args} />
-    <EyeIcon {...args} />
-    <EyeOffIcon {...args} />
-    <EditIcon {...args} />
-    <HeartIcon {...args} />
-    <RSSIcon {...args} />
-    <LoaderIcon {...args} />
-    <RefreshCCWIcon {...args} />
-    <RefreshCWIcon {...args} />
-    <PrinterIcon {...args} />
-    <CloudIcon {...args} />
-    <FileIcon {...args} />
-    <FilePlusIcon {...args} />
-    <FileMinusIcon {...args} />
-    <MenuIcon {...args} />
-    <MoreHorizontalIcon {...args} />
-    <MoreVerticalIcon {...args} />
-    <PlusIcon {...args} />
-    <MinusIcon {...args} />
-    <PowerIcon {...args} />
-    <SettingsIcon {...args} />
-    <UserPlusIcon {...args} />
-    <UserMinusIcon {...args} />
-    <UserXIcon {...args} />
-    <UsersIcon {...args} />
-    <SidebarIcon {...args} />
-    <WifiIcon {...args} />
-    <WifiOffIcon {...args} />
-    <MapIcon {...args} />
-    <AssignIcon {...args} />
-    <XIcon {...args} />
-    <CloseIcon {...args} />
-    <DangerIcon {...args} />
-    <CircleCheckIcon {...args} />
-    <CircleIcon {...args} />
-    <UploadIcon {...args} />
-    <UploadCloudIcon {...args} />
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 5rem)",
+        gridGap: "2rem",
+      }}
+    >
+      <LinkIcon {...args} />
+      <FolderIcon {...args} />
+      <LockIcon {...args} />
+      <MenuIconTwo {...args} />
+      <InboxIcon {...args} />
+      <HelpCircleIcon {...args} />
+      <InfoIcon {...args} />
+      <GlobeIcon {...args} />
+      <FolderPlusIcon {...args} />
+      <FolderMinusIcon {...args} />
+      <FileTextIcon {...args} />
+      <FilterIcon {...args} />
+      <CornerUpIcon {...args} />
+      <ExternalIcon {...args} />
+      <EditIconThree {...args} />
+      <EditIconTwo {...args} />
+      <ClipboardIcon {...args} />
+      <DatabaseIcon {...args} />
+      <CopyIcon {...args} />
+      <CloudOffIcon {...args} />
+      <CheckSquareIcon {...args} />
+      <BarChart {...args} />
+      <BarChartTwo {...args} />
+      <BellIcon {...args} />
+      <BellOffIcon {...args} />
+      <ImageIcon {...args} />
+      <ArrowLeftCircleIcon {...args} />
+      <ArrowDownCircleIcon {...args} />
+      <ArrowRightCircleIcon {...args} />
+      <ArchiveIcon {...args} />
+      <AlignRightIcon {...args} />
+      <AlignLeftIcon {...args} />
+      <AlignJustifyIcon {...args} />
+      <AlignCenterIcon {...args} />
+      <AlertTriangleIcon {...args} />
+      <ArrowUpIcon {...args} />
+      <ArrowDownIcon {...args} />
+      <ArrowLeftIcon {...args} />
+      <ArrowRightIcon {...args} />
+      <MailIcon {...args} />
+      <EyeIcon {...args} />
+      <EyeOffIcon {...args} />
+      <EditIcon {...args} />
+      <HeartIcon {...args} />
+      <RSSIcon {...args} />
+      <LoaderIcon {...args} />
+      <RefreshCCWIcon {...args} />
+      <RefreshCWIcon {...args} />
+      <PrinterIcon {...args} />
+      <CloudIcon {...args} />
+      <FileIcon {...args} />
+      <FilePlusIcon {...args} />
+      <FileMinusIcon {...args} />
+      <MenuIcon {...args} />
+      <MoreHorizontalIcon {...args} />
+      <MoreVerticalIcon {...args} />
+      <PlusIcon {...args} />
+      <MinusIcon {...args} />
+      <PowerIcon {...args} />
+      <SettingsIcon {...args} />
+      <UserPlusIcon {...args} />
+      <UserMinusIcon {...args} />
+      <UserXIcon {...args} />
+      <UsersIcon {...args} />
+      <SidebarIcon {...args} />
+      <WifiIcon {...args} />
+      <WifiOffIcon {...args} />
+      <MapIcon {...args} />
+      <AssignIcon {...args} />
+      <XIcon {...args} />
+      <CloseIcon {...args} />
+      <DangerIcon {...args} />
+      <CircleCheckIcon {...args} />
+      <CircleIcon {...args} />
+      <UploadIcon {...args} />
+      <UploadCloudIcon {...args} />
+      <FiltersLinesTwo {...args} />
+      <FiltersLinesIcon {...args} />
+      <ThreeLayersIcon {...args} />
+      <TwoLayersIcon {...args} />
+      <ZoomInIcon {...args} />
+      <ZoomOutIcon {...args} />
+      <CloseCircleIcon {...args} />
+      <UserCheckIcon {...args} />
+      <UnlockIcon {...args} />
+      <SaveIcon {...args} />
+      <SlidersIcon {...args} />
+      <ShareIcon {...args} />
+      <RotateCWIcon {...args} />
+      <RotateCCWIcon {...args} />
+      <RepeatIcon {...args} />
+      <SendIcon {...args} />
+      <SearchIcon {...args} />
+      <PaperclipIcon {...args} />
+      <MinusSquareIcon {...args} />
+      <MinusCircleIcon {...args} />
+      <DownloadIcon {...args} />
+      <DownloadCloudIcon {...args} />
+      <LoginIcon {...args} />
+      <LogoutIcon {...args} />
+      <UserIcon {...args} />
+      <InstagramIcon {...args} />
+      <InstagramIconTwo {...args} />
+      <LinkedinIcon {...args} />
+      <LinkedinIconTwo {...args} />
+      <FacebookIcon {...args} />
+      <GithubIcon {...args} />
+      <GithubIconTwo {...args} />
+      <GoogleIcon {...args} />
+      <ChevronUpIcon {...args} />
+      <ChevronDownIcon {...args} />
+      <ChevronLeftIcon {...args} />
+      <ChevronRightIcon {...args} />
+      <CalendarIcon {...args} />
+      <ClockIcon {...args} />
+      <StarIcon {...args} />
+      <SmileIcon {...args} />
+      <FrownIcon {...args} />
+      <TrashIcon {...args} />
+      <TrashIconTwo {...args} />
+      <SlashDividerIcon {...args} />
+      <HomeIcon {...args} />
+    </div>
   </StoryWrapper>
 );
 
-const Second: ComponentStory<typeof XIcon> = (args: IconProps) => (
-  <StoryWrapper>
-    <FiltersLinesTwo {...args} />
-    <FiltersLinesIcon {...args} />
-    <ThreeLayersIcon {...args} />
-    <TwoLayersIcon {...args} />
-    <ZoomInIcon {...args} />
-    <ZoomOutIcon {...args} />
-    <CloseCircleIcon {...args} />
-    <UserCheckIcon {...args} />
-    <UnlockIcon {...args} />
-    <SaveIcon {...args} />
-    <SlidersIcon {...args} />
-    <ShareIcon {...args} />
-    <RotateCWIcon {...args} />
-    <RotateCCWIcon {...args} />
-    <RepeatIcon {...args} />
-    <SendIcon {...args} />
-    <SearchIcon {...args} />
-    <PaperclipIcon {...args} />
-    <MinusSquareIcon {...args} />
-    <MinusCircleIcon {...args} />
-    <DownloadIcon {...args} />
-    <DownloadCloudIcon {...args} />
-    <LoginIcon {...args} />
-    <LogoutIcon {...args} />
-    <UserIcon {...args} />
-    <InstagramIcon {...args} />
-    <InstagramIconTwo {...args} />
-    <LinkedinIcon {...args} />
-    <LinkedinIconTwo {...args} />
-    <FacebookIcon {...args} />
-    <GithubIcon {...args} />
-    <GithubIconTwo {...args} />
-    <GoogleIcon {...args} />
-    <ChevronUpIcon {...args} />
-    <ChevronDownIcon {...args} />
-    <ChevronLeftIcon {...args} />
-    <ChevronRightIcon {...args} />
-    <CalendarIcon {...args} />
-    <ClockIcon {...args} />
-    <StarIcon {...args} />
-    <SmileIcon {...args} />
-    <FrownIcon {...args} />
-    <TrashIcon {...args} />
-    <TrashIconTwo {...args} />
-    <SlashDividerIcon {...args} />
-    <HomeIcon {...args} />
-  </StoryWrapper>
-);
+const Second: ComponentStory<typeof XIcon> = (args: IconProps) => <StoryWrapper></StoryWrapper>;
 
-export const FirstPack = First.bind({});
-FirstPack.args = {
-  width: "50px",
-  height: "50px",
-};
-
-export const SecondPack = Second.bind({});
-SecondPack.args = {
+export const Default = Template.bind({});
+Default.args = {
   width: "50px",
   height: "50px",
 };
